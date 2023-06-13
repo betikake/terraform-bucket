21e355764a03bd6f54f710e5cc77765736a7fe6f
Michael Garrett <inuendo13@gmail.com>
2023-06-13 16:04:39 +0200

    Adding terraform functions

---
+++ main.tf
@@ -0,0 +1,31 @@
+variable "project_id" {}
+variable "bucket_prefix_length" {}
+
+resource "random_id" "bucket_prefix" {
+  byte_length = var.bucket_prefix_length
+}
+
+resource "google_storage_bucket" "default" {
+  name                        = "${random_id.bucket_prefix.hex}-gcf-source" # Every bucket name must be globally unique
+  location                    = var.location
+  uniform_bucket_level_access = true
+  project                     = var.project_id
+}
+
+
+resource "google_storage_bucket_object" "default" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = var.output_location
+}
+
+
+output "bucket_name" {
+  value       = google_storage_bucket.default.name
+  description = "The name of the Google Cloud Storage bucket."
+}
+
+output "bucket_object" {
+  value       = google_storage_bucket_object.default.name
+  description = "The object path of the function source code in the Google Cloud Storage bucket."
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "location" {
+  description = "The location of the Google Cloud Storage bucket."
+  type        = string
+}
+
+variable "bucket_name" {}
+variable "source_code" {}
+
+variable "output_location" {
+  description = "The directory containing the function output location."
+  type        = string
+}
