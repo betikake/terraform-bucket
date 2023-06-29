7dc494f975d9a16fa594547949b9c65aad3e0c46
Michael Garrett <michael.garrett@betika.com>
2023-06-29 11:59:17 +0200

    Changing tf state bucket

--- main.tf
+++ main.tf
@@ -5,23 +5,23 @@ resource "random_id" "bucket_prefix" {
   byte_length = var.bucket_prefix_length
 }
 
-resource "google_storage_bucket" "default" {
+/*resource "google_storage_bucket" "default" {
   name                        = "betika-deployments-source/${var.function_name}-gcf-source" # Every bucket name must be globally unique
   location                    = var.location
   uniform_bucket_level_access = true
   project                     = var.project_id
-}
+}*/
 
 
 resource "google_storage_bucket_object" "default" {
   name   = "function-source.zip"
-  bucket = google_storage_bucket.default.name
+  bucket = "betika-deployments-source/${var.function_name}-gcf-source"
   source = var.output_location
 }
 
 
 output "bucket_name" {
-  value       = google_storage_bucket.default.name
+  value       = "betika-deployments-source/${var.function_name}-gcf-source"
   description = "The name of the Google Cloud Storage bucket."
 }
 
